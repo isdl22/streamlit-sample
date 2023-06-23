b0f1f4faff0301f436f0303c4d9ae3418dad78ff
isdl22 <34301347+isdl22@users.noreply.github.com>
2023-06-24 00:38:40 +0900

    Update example.py

--- example.py
+++ example.py
@@ -9,31 +9,32 @@ dictation = pd.read_csv('dictation_1.csv', encoding='ms949')
 
 # 랜덤한 결과 출력하는 함수
 def show_random_results():
-    # 랜덤한 4개의 운동 선택
-    random_practice = random.sample(practice['name'].tolist(), 4)
-    random_pose = random.sample(pose['pose'].tolist(), 1)
-    random_dictation = random.sample(dictation['question'].tolist(), 4)
-
-    # 새로운 데이터프레임 생성
-    selected_practice = practice[practice['name'].isin(random_practice)]
-    selected_pose = pose[pose['pose'].isin(random_pose)]
-    selected_dictation = dictation[dictation['question'].isin(random_dictation)]
-
+    # 랜덤한 2개의 대분류 선택
+    random_categories = random.sample(dictation['gubun'].unique().tolist(), 2)
+    
+    selected_dictation = pd.DataFrame()
+    
+    for category in random_categories:
+        # 대분류에 해당하는 데이터 랜덤으로 2개 선택
+        random_questions = random.sample(dictation[dictation['gubun'] == category]['question'].tolist(), 2)
+        
+        # 선택된 질문을 데이터프레임에 추가
+        selected_dictation = selected_dictation.append(dictation[(dictation['gubun'] == category) & (dictation['question'].isin(random_questions))])
+    
     # 결과 출력
     st.write("1. 실기 자세 문제:")
-    practice_results = [f"{i+1}. {name}" for i, name in enumerate(selected_practice['name'])]
+    practice_results = [f"{i+1}. {name}" for i, name in enumerate(practice['name'].sample(4))]
     st.write("\n".join(practice_results))
 
     st.write("2. 실기 포즈 문제:")
-    st.write(selected_pose.pose.unique()[0])
-
+    st.write(pose['pose'].sample(1).values[0])
 
     st.write("3. 구술 문제:")
     dictation_results = [f"{i+1}. {question}" for i, question in enumerate(selected_dictation['question'])]
     st.write("\n".join(dictation_results))
 
     st.write("\n 실기 포즈 이름:\n")
-    pose_name = [f"{i+1}. {name}" for i, name in enumerate(selected_pose['name'])]
+    pose_name = [f"{i+1}. {name}" for i, name in enumerate(pose['name'].sample(1))]
     st.write("\n".join(pose_name))
 
     st.write("\n 구술 문제 정답:\n")
