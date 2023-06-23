1eb4311a74a42b3699dd10cf247adaedbfaefdf3
isdl22 <34301347+isdl22@users.noreply.github.com>
2023-06-23 22:58:32 +0900

    Update example.py

--- example.py
+++ example.py
@@ -20,15 +20,15 @@ def show_random_results():
     selected_dictation = dictation[dictation['question'].isin(random_dictation)]
 
     # 결과 출력
-    st.write("Selected Practice:")
+    st.write("실기 자세:")
     practice_results = [f"{i+1}. {name}" for i, name in enumerate(selected_practice['name'])]
     st.write("\n".join(practice_results))
 
-    st.write("Selected Pose:")
+    st.write("실기 포즈:")
     st.write(selected_pose.pose.unique()[0])
 
 
-    st.write("Selected Dictation:")
+    st.write("구술 질문:")
     dictation_results = [f"{i+1}. {question}" for i, question in enumerate(selected_dictation['question'])]
     st.write("\n".join(dictation_results))
 
