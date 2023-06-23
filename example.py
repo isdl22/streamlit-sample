29d5cab5f14e642e2b3cd416620c054a6aa4b1e2
isdl22 <34301347+isdl22@users.noreply.github.com>
2023-06-23 23:07:05 +0900

    Update example.py

--- example.py
+++ example.py
@@ -33,7 +33,7 @@ def show_random_results():
     st.write("\n".join(dictation_results))
 
     st.write("\nSelected Pose answer:\n")
-    pose_name = [f"{i+1}.{name}" for i, name in enumerate(selected_pose['name'])]
+    pose_name = [f"{i+1}. {name}" for i, name in enumerate(selected_pose['name'])]
     st.write("\n".join(pose_name))
 
     st.write("\nSelected Dictation answer:\n")
