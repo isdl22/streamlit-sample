cc8a558a750cf8147a26a9083e0dc50272034e7e
isdl22 <34301347+isdl22@users.noreply.github.com>
2023-06-30 15:13:39 +0900

    Update example.py

--- example.py
+++ example.py
@@ -25,29 +25,29 @@ def show_random_results():
     random_pose = random.sample(pose['pose'].tolist(), 1)
 
     # 규정 중에서 랜덤 질문 2개 선택
-    random_dictation_1 = random.sample(dictation[dictation['gubun'].isin(['규정'])]['question'].tolist(), 2)
+    #random_dictation_1 = random.sample(dictation[dictation['gubun'].isin(['규정'])]['question'].tolist(), 2)
 
     # 지도방법 중에서 랜덤 질문 2개 선택
-    random_dictation_2 = random.sample(dictation[dictation['gubun'].isin(['지도방법'])]['question'].tolist(), 2)
+    #random_dictation_2 = random.sample(dictation[dictation['gubun'].isin(['지도방법'])]['question'].tolist(), 2)
 
     
     # 협회최신규정, 종목소개 중에서 랜덤 질문 1개 선택
-    #random_dictation_1 = random.sample(dictation[dictation['gubun_1'].isin(['협회최신규정', '종목소개'])]['question'].tolist(), 1)
+    random_dictation_1 = random.sample(dictation[dictation['gubun_1'].isin(['협회최신규정', '종목소개'])]['question'].tolist(), 1)
 
     # 스포츠 인권, 생활체육 개요 중에서 랜덤 질문 1개 선택
-    #random_dictation_2 = random.sample(dictation[dictation['gubun_1'].isin(['스포츠 인권', '생활체육 개요'])]['question'].tolist(), 1)
+    random_dictation_2 = random.sample(dictation[dictation['gubun_1'].isin(['스포츠 인권', '생활체육 개요'])]['question'].tolist(), 1)
 
-    # 웨이트트레이닝, 과학적 지도방법 중에서 랜덤 질문 1개 선택
-    #random_dictation_3 = random.sample(dictation[dictation['gubun_1'].isin(['웨이트트레이닝', '과학적 지도방법'])]['question'].tolist(), 1)
+    # 웨이트트레이닝, 규정포즈 중에서 랜덤 질문 1개 선택
+    random_dictation_3 = random.sample(dictation[dictation['gubun_1'].isin(['웨이트트레이닝', '규정포즈'])]['question'].tolist(), 1)
 
-    # 규정포즈, 응급처치 중에서 랜덤 질문 1개 선택
-    #random_dictation_4 = random.sample(dictation[dictation['gubun_1'].isin(['규정포즈', '응급처치'])]['question'].tolist(), 1)
+    # 과학적 지도방법, 응급처치 중에서 랜덤 질문 1개 선택
+    random_dictation_4 = random.sample(dictation[dictation['gubun_1'].isin(['과학적 지도방법', '응급처치'])]['question'].tolist(), 1)
 
     # 새로운 데이터프레임 생성
     selected_practice = practice[practice['name'].isin(random_practice_1 + random_practice_2 + random_practice_3 + random_practice_4)]
     selected_pose = pose[pose['pose'].isin(random_pose)]
-    selected_dictation = dictation[dictation['question'].isin(random_dictation_1 + random_dictation_2)]
-    #selected_dictation = dictation[dictation['question'].isin(random_dictation_1 + random_dictation_2 + random_dictation_3 + random_dictation_4)]
+    #selected_dictation = dictation[dictation['question'].isin(random_dictation_1 + random_dictation_2)]
+    selected_dictation = dictation[dictation['question'].isin(random_dictation_1 + random_dictation_2 + random_dictation_3 + random_dictation_4)]
 
     # 결과 출력
     st.write("1. 실기 자세 문제:")
